175ef380f4666f3eb4cdc6bb2f8035b59bbf77e0
Jakub Faleńczyk <jakubfalenczyk@gmail.com>
2020-06-06 13:55:19 +0200

    updating credits modal

--- infinite-clicker-app/src/components/UserInterface/components/Settings/components/Credits.tsx
+++ infinite-clicker-app/src/components/UserInterface/components/Settings/components/Credits.tsx
@@ -11,27 +11,34 @@ const Credits = () => {
     <>
       <Button onClick={onOpen}>Credits</Button>
       <GameModal 
+        className="creditsModal"
         title="Credits"
         isOpen={isOpen}
         onClose={onClose}
         isNested={true}
         actions={<Button onClick={onClose}>Ok</Button>}
       >
-        This game has been created by Jakub Falenczyk<br/>
+        This game has been created by Jakub Falenczyk<br/><br/>
         You can find me on LinkedIn:<br/>
         <a className="creditsLink" href="https://linkedin.com/in/jakubfalenczyk" target="_blank" rel="noopener noreferrer">
           https://linkedin.com/in/jakubfalenczyk
         </a>
         <br/>
+        If you're interested in learning web development while building browser games, I encourage you to visit my website:
+        <a className="creditsLink" href="https://programmingrush.com/" target="_blank" rel="noopener noreferrer">
+        https://programmingrush.com/
+        </a>
+        <br/>
         <span>
-          I'm using assets from:<br/>
+          This game is using assets from:<br/>
           <a className="creditsLink" href="https://kenney.nl" target="_blank" rel="noopener noreferrer">
             kenney.nl
           </a><br/>
           <a className="creditsLink" href="https://www.vecteezy.com/free-vector/termite">Vecteezy.com</a>
         </span>
+        <br/>
         <span>
-        and music from:<br/>
+        and music/sounds from:<br/>
           <a className="creditsLink" href="https://zapsplat.com/" target="_blank" rel="noopener noreferrer">
             https://zapsplat.com/
           </a>
